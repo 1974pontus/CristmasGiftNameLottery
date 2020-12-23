0a4557f18ee939d01343824fb55f282dda008489
Pontus Tahir <pontus.tahir@gmail.com>
2020-12-23 15:56:22 +0100

    kind of working

---
+++ index.js
@@ -0,0 +1,34 @@
+const ul = document.querySelector('ul');
+const container = document.querySelector('.container');
+const showName = document.createElement('h2');
+const players = [];
+
+
+function names() {
+  let inputName = document.getElementById('inputName').value;
+  players.push(inputName);
+  console.log(players);
+
+  let li = document.createElement('li');
+  li.innerText = inputName;
+  ul.appendChild(li);
+}
+
+
+function nextRound() {
+  while (players.length) {
+    let player = Math.floor(Math.random() * players.length);
+    showName.innerText = players[player];
+    showName.style.color = 'red'
+    container.appendChild(showName)
+    console.log(players[player]);
+    players.splice(player, 1);
+    console.log(players)
+    ul.removeChild(ul.childNodes[player])
+    if (showName === player) {
+      console.log('func 2')
+      container.removeChild(showName)
+    }
+    break;
+  };
+};
